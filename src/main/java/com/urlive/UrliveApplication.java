eaa2bc344df7aa333d3a0ebcf87a883ed14997ad
bingsuzoa <zxc_777@naver.com>
2025-05-26 20:12:37 +0900

    Inital commit

---
+++ src/main/java/com/urlive/UrliveApplication.java
@@ -0,0 +1,13 @@
+package com.urlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrliveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urlive/UrliveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
