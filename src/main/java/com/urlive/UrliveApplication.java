9e2a4b1e60276f302e0124cc0ff0a6a3f7bdeab9
bingsuzoa <zxc_777@naver.com>
2025-06-06 07:41:37 +0900

    test : webMvcTest 완성

--- src/main/java/com/urlive/UrliveApplication.java
+++ src/main/java/com/urlive/UrliveApplication.java
@@ -2,10 +2,12 @@ package com.urlive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@Profile("!test")
 public class UrliveApplication {
 
 	public static void main(String[] args) {
